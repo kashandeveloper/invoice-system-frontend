28de876eb17cde2d6d62287590657b6880427716
kashandeveloper <kashanmalikhabib142@gmail.com>
2026-03-03 20:19:56 +0500

    Update reports.js

--- assets/js/reports.js
+++ assets/js/reports.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://kashan.42web.io/api";
+// const API_BASE = "https://kashan.42web.io/api";
 document.addEventListener("DOMContentLoaded",async()=>{const tbody=document.querySelector("#reportTable tbody");
 const data = await jsonFetch(`${API_BASE}/invoices/reports.php`);
 tbody.innerHTML="";
